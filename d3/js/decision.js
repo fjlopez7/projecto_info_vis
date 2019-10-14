17dca3c2cf4732da1c3cff24698272d20756f7b5
KnowYourselves <rsalvarez@uc.cl>
2019-10-14 11:44:23 -0300

    Advancing

--- d3/js/decision.js
+++ d3/js/decision.js
@@ -1,6 +1,5 @@
 const parse_name = (name) => name.split(" ").join("-")
 
-
 // Función que crea el gráfico. Recibe la url del json, el tamaño y el tipo.
 const create = (url, diameter, type) => {
   d3.csv(url).then(data => {
@@ -28,38 +27,70 @@ const create = (url, diameter, type) => {
       .attr('width', diameter)
       .attr('height', diameter)
       .attr('class', 'bubble');
-    
+
+    const mouseout = d => {
+      console.log(d)
+      svg.select('.highlight circle')
+        .transition(`RemoveHighlight-${d.data.Name}`)
+        .duration(100)
+        .attr('r', d.r)
+        .on('end', _ => svg.select('g.highlight').remove())
+    }
+
+    const mouseover = d => {
+      highligth_g = svg
+        .append('g')
+        .attr('transform', `translate(${d.x}, ${d.y})`)
+        .classed('highlight', true)
+
+      highligth_g.append('circle')
+        .attr('r', d.r)
+        .style('fill', color(d.data.Name))
+        .transition(`AddHighlight-${d.data.Name}`)
+        .duration(100)
+        .attr('r', d.r * 1.5)
+
+      highligth_g.append('text')
+        .attr('dy', '.2em')
+        .style('text-anchor', 'middle')
+        .text(d.data.Name)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', d => 10) // Debería ser dinámico
+        .attr('fill', 'white');
+
+      highligth_g.append('text')
+        .attr('dy', '1.3em')
+        .style('text-anchor', 'middle')
+        .text(d.data.Count)
+        .attr('font-family', 'Gill Sans', 'Gill Sans MT')
+        .attr('font-size', d => 10) // Debería ser dinámico
+        .attr('fill', 'white');
+
+      highligth_g.on('mouseout', _ => mouseout(d))
+    }
+
+
     // Creamos los nodos
     node = svg.selectAll('.node')
-      .data(bubble(nodes).descendants())
+      .data(bubble(nodes).descendants(), d => d.data.Name)
       .enter()
       .filter(d => !d.children && d.r) // Esto ignora los agrupamientos generales (comentar para ver a que me refiero)
       .append('g')
       .attr('transform', d => `translate(${d.x},${d.y})`)
-    
-    console.log(node);
 
     // Les damso título (tooltip)
     node.append('title')
       .text(d => `${d.data.Name}: ${d.data.Count}`)
 
+
     // Coloreamos los círculos
     node.append('circle')
       .attr('r', d => d.r)
-      .style('fill', (d, i) => color(i))
+      .style('fill', (d) => {
+        return color(d.data.Name)
+      })
       .attr('class', d => `node ${parse_name(d.data.Name)}`)  // Parseamos el nombre y lo asignamos como clase
-      .on('mouseover', d => {
-        svg.select(`.${parse_name(d.data.Name)}`)
-          .transition()
-            .duration(100)
-            .attr('r', d.r * 1.5)
-      })
-      .on('mouseout', d => {
-        svg.select(`.${parse_name(d.data.Name)}`)
-          .transition()
-            .duration(100)
-            .attr('r', d.r)
-      })
+      .on('mouseover', mouseover)
 
     // Escribimos el texto y el tamaño
     node.append('text')
@@ -69,7 +100,7 @@ const create = (url, diameter, type) => {
       .attr('font-family', 'sans-serif')
       .attr('font-size', d => 10) // Debería ser dinámico
       .attr('fill', 'white');
-    
+
     node.append('text')
       .attr('dy', '1.3em')
       .style('text-anchor', 'middle')
