8fca04d8019a2d1a1cafb19371a142af1fd68f5a
KnowYourselves <rsalvarez@uc.cl>
2019-10-14 01:18:00 -0300

    :tada: Fixed bubble chart

--- d3/js/decision.js
+++ d3/js/decision.js
@@ -1,50 +1,68 @@
 
+// Función que crea el gráfico. Recibe la url del json y el tamaño.
+const create = (url, diameter) => {
+  d3.csv(url).then(data => {
+    // Adaptamos el
+    const dataset = {
+      children: data.slice(0, data.length).map(d => {
+        return { Name: d.genre, Count: d.numberclassified }
+      })
+    };
 
+    // Creamos la escala de color
+    color = d3.scaleOrdinal(d3.schemeCategory10);
 
+    // Creamos pack (similar a como creamos tree en la tarea)
+    bubble = d3.pack()
+      .size([diameter, diameter])
+      .padding(1.5);
+
+    // Creamos los nodos usando hierarchy
+    nodes = d3.hierarchy(dataset).sum(d => d.Count)
+
+    // Creamos el SVG
+    svg = d3.select("body")
+      .append("svg")
+      .attr("width", diameter)
+      .attr("height", diameter)
+      .attr("class", "bubble");
+
+    // Creamos los nodos
+    node = svg.selectAll(".node")
+      .data(bubble(nodes).descendants())
+      .enter()
+      .filter(d => !d.children) // Esto ignora los agrupamientos generales (comentar para ver a que me refiero)
+      .append("g")
+      .attr("class", "node")
+      .attr("transform", d => `translate(${d.x},${d.y})`)
+
+    // Les damso título (tooltip)
+    node.append("title")
+      .text(d => `${d.data.Name}: ${d.data.Count}`)
+
+    // Coloreamos los círculos
+    node.append("circle")
+      .attr("r", d => d.r)
+      .style("fill", (d, i) => color(i))
+
+    // Escribimos el texto y el tamaño
+    node.append("text")
+      .attr("dy", ".2em")
+      .style("text-anchor", "middle")
+      .text(d => d.data.Name)
+      .attr("font-family", "sans-serif")
+      .attr("font-size", d => 10) // Debería ser dinámico
+      .attr("fill", "white");
+    
+    node.append("text")
+      .attr("dy", "1.3em")
+      .style("text-anchor", "middle")
+      .text(d => d.data.Count)
+      .attr("font-family", "Gill Sans", "Gill Sans MT")
+      .attr("font-size", d => 10) // Debería ser dinámico
+      .attr("fill", "white");
+  })
+}
 
 const filepath = "https://raw.githubusercontent.com/KnowYourselves/Infovis-T02-Dataset/master/stats_decision_forest.csv";
-const promise_1 = d3.csv(filepath);
-let data;
-promise_1.then(value=> {
-    return value;
-    });
-
-console.log(promise_1);
-const color = data => d3.scaleOrdinal(data.map(d => d.name), d3.schemeCategory10); 
-
-const data_1 = data.slice(0, data.length).map(d => {
-    return {name: d.genre, value: d.numberclassified}
-  });
-
-const pack = data => d3.pack()
-            .size([width - 2, height - 2])
-            .padding(3)(d3.hierarchy({children: data}).sum(d => d.value));
-
-const root = pack(data_1);
-  
-const svg = d3.select("#decision")
-    .append("svg")
-    .attr("viewBox", [0, 0, width, height])
-    .attr("font-size", 20)
-    .attr("font-family", "sans-serif")
-    .attr("text-anchor", "middle");
-
-const leaf = svg.selectAll("g")
-    .data(root.leaves())
-    .enter()
-        .append("g")
-        .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
-
-leaf.append("circle")
-    .attr("id", d => (d.leafUid = DOM.uid("leaf")).id)
-    .attr("r", d => d.r)
-    .attr("fill-opacity", 0.7)
-    .attr("fill", d => color(d.data.name));
-
-leaf.append("text")
-    .selectAll("tspan")
-    .data(d => d.data.name.split(/(?=[A-Z][^A-Z])/g))
-    .join("tspan")
-        .attr("x", 0)
-        .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
-        .text(d => d);
+create(filepath, 500)
